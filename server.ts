695ace35f0c0636a3c9c6c3616eeb79bed8fb17b
lwhela12 <lucas@whelanpartners.com>
2026-09-14 14:18:42 -0700

    Serve generated frontend assets in clean Vercel builds

--- server.ts
+++ server.ts
@@ -2,5 +2,6 @@ import express from "express";
 import path from "node:path";
 import { createApp } from "./server/app.js";
 const app: express.Express = createApp();
+app.use(express.static(path.join(process.cwd(), "public")));
 app.get("*", (_req, res) => res.sendFile(path.join(process.cwd(), "public", "index.html")));
 export default app;
